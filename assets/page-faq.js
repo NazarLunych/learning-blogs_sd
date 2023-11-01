5e96eae0a993145f8e136891398d4e93458674bd
Nazarii Lunych <Nazariy.lunich@gmail.com>
2023-11-01 18:18:36 +0200

    Code modification

--- assets/page-faq.js
+++ assets/page-faq.js
@@ -1,28 +1,42 @@
 function initFAQPage() {
-  const faqAccordions = document.querySelectorAll(".js-faq-accordion");
+  const selectors = {
+    faqAccordion: ".js-faq-accordion",
+    sectionHeader: ".section-header",
+    faqMenu: ".js-faq-menu",
+    faqLinks: ".js-faq-links"
+  };
+
+  const classes = {
+    faqAccordionActive: "faq__accordion--active",
+    faqBtnActive: "faq__btn--active",
+    sectionHeaderHidden: "shopify-section-header-hidden",
+    faqMenuOffset: "faq__menu--offset"
+  };
+
+  const faqAccordions = document.querySelectorAll(selectors.faqAccordion);
   faqAccordions.length && faqAccordions.forEach((button) => {
     button.addEventListener("click", (e) => {
       const notAnswer = e.target.closest("div").dataset.target !== "answer";
-      notAnswer && button.classList.toggle("faq__accordion--active");
+      notAnswer && button.classList.toggle(classes.faqAccordionActive);
     });
   });
 
   const changeActiveLink = (index) => {
-    faqLinks.forEach((link, linkIndex) => link.classList.toggle("faq__btn--active", linkIndex === index));
+    faqLinks.forEach((link, linkIndex) => link.classList.toggle(classes.faqBtnActive, linkIndex === index));
   };
 
   let isHeaderHidden = false;
   const yOffset = 117;
-  const header = document.querySelector(".section-header");
-  const faqMenu = document.querySelector(".js-faq-menu");
-  const faqLinks = document.querySelectorAll(".js-faq-links");
+  const header = document.querySelector(selectors.sectionHeader);
+  const faqMenu = document.querySelector(selectors.faqMenu);
+  const faqLinks = document.querySelectorAll(selectors.faqLinks);
   faqLinks.length && faqLinks.forEach((link, index) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
 
       setTimeout(() => {
-        isHeaderHidden = header.classList.contains("shopify-section-header-hidden");
-        faqMenu.classList.toggle("faq__menu--offset", !isHeaderHidden);
+        isHeaderHidden = header.classList.contains(classes.sectionHeaderHidden);
+        faqMenu.classList.toggle(classes.faqMenuOffset, !isHeaderHidden);
       }, 0);
 
       const targetElem = document.getElementById(link.dataset.target);
@@ -36,8 +50,8 @@ function initFAQPage() {
   });
 
   window.addEventListener("scroll", () => {
-    isHeaderHidden = header.classList.contains("shopify-section-header-hidden");
-    faqMenu.classList.toggle("faq__menu--offset", !isHeaderHidden);
+    isHeaderHidden = header.classList.contains(classes.sectionHeaderHidden);
+    faqMenu.classList.toggle(classes.faqMenuOffset, !isHeaderHidden);
   });
 }
 
