e659b060eddd5c3fda988fc9071813d364eceabc
Nazarii Lunych <Nazariy.lunich@gmail.com>
2023-11-17 15:37:22 +0200

    Comments fixes

--- assets/custom-video.js
+++ assets/custom-video.js
@@ -3,58 +3,67 @@ function customVideoFuncInit() {
     playBtn: ".js-custom-video-play-btn",
     video: ".js-custom-video",
     iframeVideo: ".js-custom-video-iframe",
-    videoWrapper: ".js-custom-video-wrapper",
     modal: ".js-custom-video-modal",
     iframePoster: ".js-custom-video-iframe-poster"
   };
 
   const classes = {
     hiddenPlayBtn: "custom-video__play-btn--hidden",
-    customHidden: "custom-video__hidden",
+    customHidden: "custom-video__modal--hidden",
     hidden: "hidden"
   };
 
   const playBtn = document.querySelector(selectors.playBtn);
-  const videos = document.querySelectorAll(selectors.video);
+  const video = document.querySelector(selectors.video);
   const modal = document.querySelector(selectors.modal);
-  const videoWrapper = document.querySelector(selectors.videoWrapper);
-  const iframes = document.querySelectorAll(selectors.iframeVideo);
-  const iframePosters = document.querySelectorAll(selectors.iframePoster);
+  const iframe = document.querySelector(selectors.iframeVideo);
+  const iframePoster = document.querySelector(selectors.iframePoster);
+
+  const videoHandler = (play) => {
+    if (video) {
+      if (play) {
+        video.play();
+        video.controls = true;
+      } else {
+        video.pause();
+        video.load();
+        video.controls = false;
+      }
+    } else if (iframe) {
+      const isYoutube = iframe.src.includes("youtube");
+      iframe.src += (isYoutube ? "?mute=1" : "?muted=1") + `&autoplay=${+play}`;
+    }
+  };
 
   const onPopupClose = () => {
-    videoWrapper.classList.remove(classes.customHidden);
-    modal.classList.add(classes.customHidden);
+    videoHandler(false);
+
+    playBtn?.classList.remove(classes.hiddenPlayBtn);
+    modal?.classList.add(classes.customHidden);
+    iframePoster?.classList.remove(classes.hidden);
     document.getElementsByTagName("body")[0].style.overflow = "visible";
   };
 
   const onPlayClick = () => {
-    const isPopup = videoWrapper.dataset.videoPositionType === "popup";
+    const isPopup = modal.dataset.videoPositionType === "popup";
 
-    iframePosters.length && iframePosters.forEach((poster) => {
-      poster.classList.add(classes.hidden);
-    });
+    videoHandler(true);
 
-    iframes.length && iframes.forEach((video) => {
-      const isYoutube = video.src.includes("youtube");
-      video.src += (isYoutube ? "?mute=1" : "?muted=1") + "&autoplay=1";
-    });
-
-    videos.length && videos.forEach(video => video.play());
     playBtn.classList.add(classes.hiddenPlayBtn);
+    iframePoster?.classList.add(classes.hidden);
 
     if (isPopup) {
-      videoWrapper.classList.add(classes.customHidden);
-      modal.classList.remove(classes.customHidden);
+      modal?.classList.remove(classes.customHidden);
 
-      modal && modal.addEventListener("click", (e) => {
-        e.target.dataset.target === "modal" && onPopupClose();
+      modal?.addEventListener("click", (e) => {
+        modal === e.target && onPopupClose();
       });
 
       document.getElementsByTagName("body")[0].style.overflow = "hidden";
     }
   };
 
-  playBtn && playBtn.addEventListener("click", onPlayClick);
+  playBtn?.addEventListener("click", onPlayClick);
 }
 
 customVideoFuncInit();
